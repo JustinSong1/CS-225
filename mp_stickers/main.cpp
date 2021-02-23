799e714dfab7b3065a22005d917467c7219a39a1
Justin Song <justinsong18@gmail.com>
2021-02-22 19:44:31 -0600

    Wrote main and made myImage

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -3,10 +3,32 @@
 
 int main() {
 
-  //
-  // Reminder:
-  //   Before exiting main, save your creation to disk as myImage.png
-  //
+    //
+    // Reminder:
+    //   Before exiting main, save your creation to disk as myImage.png
+    //
 
-  return 0;
+    Image court;
+    court.readFromFile("court.png");
+
+    Image steph;
+    steph.readFromFile("steph.png");
+    steph.scale(0.4);
+
+    Image lebron;
+    lebron.readFromFile("lebron.png");
+    lebron.scale(0.25);
+
+    Image trophy;
+    trophy.readFromFile("trophy.png");
+    trophy.scale(0.3);
+
+    StickerSheet sheet(court, 5);
+
+    sheet.addSticker(steph, 100, 300);
+    sheet.addSticker(lebron, 500, 100);
+    sheet.addSticker(trophy, 400, 300);
+
+    sheet.render().writeToFile("myImage.png");
+    return 0;
 }
