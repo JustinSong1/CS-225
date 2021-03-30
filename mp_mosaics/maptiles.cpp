3d73036b67d5de21ec5c56b6a0e591de7608d047
Justin Song <justinsong18@gmail.com>
2021-03-29 21:39:14 -0500

    Wrote maptiles

--- mp_mosaics/maptiles.cpp
+++ mp_mosaics/maptiles.cpp
@@ -18,10 +18,28 @@ Point<3> convertToXYZ(LUVAPixel pixel) {
 MosaicCanvas* mapTiles(SourceImage const& theSource,
                        vector<TileImage>& theTiles)
 {
-    /**
-     * @todo Implement this function!
-     */
+    int rows = theSource.getRows();
+    int cols = theSource.getColumns();
+    MosaicCanvas* canvas = new MosaicCanvas(rows, cols);
+    vector<Point<3>> points;
+    map<Point<3>, TileImage> tileMap;
+    points.reserve(theTiles.size());
+    for (size_t i = 0; i < theTiles.size(); i++) {
+        LUVAPixel avg = theTiles[i].getAverageColor();
+        Point<3> point = convertToXYZ(avg);
+        points.push_back(point);
+        tileMap[point] = theTiles[i];
+    }
 
-    return NULL;
+    KDTree<3> pointsTree(points);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            Point<3> target = convertToXYZ(theSource.getRegionColor(i, j));
+            Point<3> replacement = pointsTree.findNearestNeighbor(target);
+            canvas -> setTile(i, j, &tileMap[replacement]);
+        }
+    }
+    return canvas;
 }
 
