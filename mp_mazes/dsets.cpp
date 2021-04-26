e102535904430c8b104eb314df0caa3c56c2497b
Justin Song <justinsong18@gmail.com>
2021-04-26 17:40:37 -0500

    Finished part 1 of mp mazes

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,36 @@
 /* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num) {
+    for(int i = 0; i < num; i++) {
+        data.push_back(-1);
+    }
+}
+
+int DisjointSets::find(int elem) {
+    if(data[elem] < 0) {
+        return elem;
+    } else {
+        data[elem] = find(data[elem]);
+        return data[elem];
+    }
+}
+
+void DisjointSets::setunion(int a, int b) {
+    int rootA = find(a);
+    int rootB = find(b);
+    int newSize = data[rootA] + data[rootB];
+    if(rootA == rootB) return;
+    if(data[rootA] > data[rootB]) {
+        data[rootA] = rootB;
+        data[rootB] = newSize;
+    } else {
+        data[rootB] = rootA;
+        data[rootA] = newSize;
+    }
+}
+
+int DisjointSets::size(int elem) {
+    return -1*data[find(elem)];
+}
+
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,12 @@
-/* Your code here! */
+#include <vector>
+class DisjointSets {
+public:
+    //Creates n unconnected root nodes at the end of the vector.
+    void addelements(int num);
+    //This function should compress paths and works as described in lecture.
+    int find(int elem);
+    void setunion(int a, int b);
+    int size(int elem);
+private:
+    std::vector<int> data;
+};
